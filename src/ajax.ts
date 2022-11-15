e65524602320b29ea827b05f6346099478a9e1b5
Mojtaba Zarei <zarei.bs@gmail.com>
2022-11-15 15:51:11 +0330

    added userAgent to the context

--- src/ajax.ts
+++ src/ajax.ts
@@ -3,19 +3,24 @@ import { ActionType, EventType } from './types'
 import config from './config'
 
 const baseUrl = "https://stage-api.mydeepin.ir/v1/"
+
 export function sendRequest(eventsArray: EventType[]): Promise<any> {
     let body: any = eventsArray[0]
     let endpoint = body.type
 
     if (eventsArray.length > 1) {
-        endpoint = ActionType.Batch,
-            body = {
-                type: ActionType.Batch,
-                batch: eventsArray
-            }
+        endpoint = ActionType.Batch
+        body = {
+            type: ActionType.Batch,
+            batch: eventsArray,
+        }
     }
+    body.context = {
+        agent: navigator.userAgent
+    }
+    const url = baseUrl + endpoint
 
-    return fetch(baseUrl + endpoint, {
+    return fetch(url, {
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json',
