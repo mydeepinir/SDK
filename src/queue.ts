8d60e2257f4f6611578eb6580608f87e0b9000c1
Mojtaba Zarei <zarei.bs@gmail.com>
2022-11-27 20:14:26 +0330

    added queue class

--- src/queue.ts
+++ src/queue.ts
@@ -2,35 +2,65 @@ import { ActionType, EventType } from "./types"
 import { storage, LocalStorage } from "./storage"
 import config from "./config"
 import { sendRequest } from './ajax'
-import { getDeviceId, getIso8601 } from "./utils"
+import { getIso8601 } from "./utils"
+import { deviceId } from "./deviceId"
 
 const BUNCH_KEY = 'DeepinEventsBulk'
+
+const localStorageIsAvailable = LocalStorage.available()
+class Queue {
+    items: any[] = []
+    lockTimeItems: any[] = []
+    locked: boolean = false
+    constructor() {
+        this.items = storage.get(BUNCH_KEY, [])
+    }
+
+    add(item: any) {
+        if (this.locked) {
+            this.lockTimeItems.push(item)
+        }
+        this.items.push(item)
+        storage.set(BUNCH_KEY, this.items)
+    }
+
+    clear() {
+        this.items = []
+        storage.set(BUNCH_KEY, this.items)
+    }
+    lock() {
+        this.locked = true
+        this.lockTimeItems = []
+    }
+    unlock() {
+        this.locked = false
+        this.items = [...this.lockTimeItems]
+    }
+}
+const queue = new Queue();
 
 function releaseCondition(items: EventType[]) {
     return items.length > 5
 }
 
-export function sendInQueue(actionType: ActionType, eventData: any, eventObject: EventType): Promise<any> {
-    if (!config.isConfigured()) { return Promise.reject() }
+export async function sendInQueue(actionType: ActionType, eventData: any, eventObject: EventType): Promise<any> {
+    if (config.checkError()) { return Promise.reject(config.checkError()) }
 
     const event = {
         type: actionType,
         ...eventObject,
         ...eventData,
-        deviceId: getDeviceId(),
+        deviceId,
         timestamp: getIso8601(),
     }
 
-    const localStorageIsAvailable = LocalStorage.available()
-    let items: any[] = localStorageIsAvailable ? storage.get(BUNCH_KEY) || [] : []
-    items.push(event)
-    const releaseEventsStack = config.configuration.dontBunch || !localStorageIsAvailable || releaseCondition(items)
-    if (localStorageIsAvailable) {
-        storage.set(BUNCH_KEY, releaseEventsStack ? [] : items)
-    }
+    queue.add(event)
+    const releaseEventsStack = deviceId && (config.configuration.dontBunch || !localStorageIsAvailable || releaseCondition(queue.items))
+
     if (releaseEventsStack) {
-        return sendRequest(items)
-    } else {
-        return Promise.resolve()
+        queue.lock()
+        await sendRequest(queue.items)
+        queue.clear()
+        queue.unlock()
     }
 }
